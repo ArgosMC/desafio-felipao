f68feb859485050e09973142634b1b7ba3272302
Alessandro <168305037+ArgosMC@users.noreply.github.com>
2024-06-07 05:12:25 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-let nome = "Alessandro";
-console.log("nome");
+let nomeHeroi = "SuperMan";
+let xpHeroi = 8500;
+let nivel;
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1000 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 6000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
